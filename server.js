3985ffaca3c765f5f0e3bf7f1765cd6e5f8beec1
Hawijo <joshuahawi24@gmail.com>
2025-05-20 12:02:48 +0300

    Adding database connection

--- server.js
+++ server.js
@@ -31,15 +31,36 @@ app.route('/').get((req, res) => {
   res.render('index', { title: 'Hello', message: 'Please log in' });
 });
 
-passport.serializeUser((user, done) => {
+
+myDB(async client => {
+  const myDataBase = await client.db('database').collection('users');
+
+  // Be sure to change the title
+  app.route('/').get((req, res) => {
+    // Change the response to render the Pug template
+    res.render('index', {
+      title: 'Connected to Database',
+      message: 'Please login'
+    });
+  });
+
+  passport.serializeUser((user, done) => {
   done(null, user._id);
 });
 
 passport.deserializeUser((id, done) => {
-  // myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
-    done(null, null);
-  // });
+  myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
+    done(null, doc);
+   });
 });
+
+  // Be sure to add this...
+}).catch(e => {
+  app.route('/').get((req, res) => {
+    res.render('index', { title: e, message: 'Unable to connect to database' });
+  });
+});
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
