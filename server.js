b17c04eb719e8ef890916b8da2b46d589b957af9
Hawijo <joshuahawi24@gmail.com>
2025-05-23 13:47:51 +0300

    Adding social login

--- server.js
+++ server.js
@@ -1,84 +1,45 @@
-const passport = require("passport");
-const bcrypt = require("bcrypt");
+'use strict';
+require('dotenv').config();
+const express = require('express');
+const myDB = require('./connection');
+const fccTesting = require('./freeCodeCamp/fcctesting.js');
+const session = require('express-session');
+const passport = require('passport');
+const routes = require('./routes.js');
+const auth = require('./auth.js');
 
-module.exports = function (app, myDataBase) {
-  app.route("/").get((req, res) => {
-    res.render("index", {
-      title: "Connected to Database",
-      message: "Please log in",
-      showLogin: true,
-      showRegistration: true,
-      showSocialAuth: true,
-    });
+const app = express();
+
+app.set('view engine', 'pug');
+app.set('views', './views/pug');
+
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  cookie: { secure: false }
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+fccTesting(app); // For fCC testing purposes
+app.use('/public', express.static(process.cwd() + '/public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+myDB(async client => {
+  const myDataBase = await client.db('database').collection('users');
+
+  routes(app, myDataBase);
+  auth(app, myDataBase);
+}).catch(e => {
+  app.route('/').get((req, res) => {
+    res.render('index', { title: e, message: 'Unable to connect to database' });
   });
-
-  app
-    .route("/login")
-    .post(
-      passport.authenticate("local", { failureRedirect: "/" }),
-      (req, res) => {
-        res.redirect("/profile");
-      }
-    );
-
-  app.route("/profile").get(ensureAuthenticated, (req, res) => {
-    res.render("profile", { username: req.user.username });
-  });
-
-  app.route("/logout").get((req, res) => {
-    req.logout();
-    res.redirect("/");
-  });
-
-  app.route("/register").post(
-    (req, res, next) => {
-      const hash = bcrypt.hashSync(req.body.password, 12);
-      myDataBase.findOne({ username: req.body.username }, (err, user) => {
-        if (err) {
-          next(err);
-        } else if (user) {
-          res.redirect("/");
-        } else {
-          myDataBase.insertOne(
-            {
-              username: req.body.username,
-              password: hash,
-            },
-            (err, doc) => {
-              if (err) {
-                res.redirect("/");
-              } else {
-                next(null, doc.ops[0]);
-              }
-            }
-          );
-        }
-      });
-    },
-    passport.authenticate("local", { failureRedirect: "/" }),
-    (req, res, next) => {
-      res.redirect("/profile");
-    }
-  );
-
-  app.route("/auth/github").get(passport.authenticate("github"));
-  app
-    .route("/auth/github/callback")
-    .get(
-      passport.authenticate("github", { failureRedirect: "/" }),
-      (req, res) => {
-        res.redirect("/profile");
-      }
-    );
-
-  app.use((req, res, next) => {
-    res.status(404).type("text").send("Not Found");
-  });
-};
-
-function ensureAuthenticated(req, res, next) {
-  if (req.isAuthenticated()) {
-    return next();
-  }
-  res.redirect("/");
-}
+});
+  
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
